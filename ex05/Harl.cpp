65007b83d3fe363cf644a514110fb4c9c596528c
MaryTim <iammariatimoshenko@gmail.com>
2024-12-16 18:58:43 +0100

    ex05 wip

---
+++ ex05/Harl.cpp
@@ -0,0 +1,9 @@
+#include "Harl.hpp"
+
+Harl::Harl() { }
+
+Harl::~Harl() { }
+
+void Harl::debug() {
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
